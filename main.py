ec928b9bd562693a264231aea3c85767f03cbe5e
molanjad2011 <thus.is.zero.point@gmail.com>
2025-12-30 15:01:54 +0330

    fix

--- main.py
+++ main.py
@@ -6,150 +6,51 @@ gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk, Pango, Gdk
 
 sura_verses = {
-    1: 7,
-    2: 286,
-    3: 200,
-    4: 176,
-    5: 120,
-    6: 165,
-    7: 206,
-    8: 75,
-    9: 129,
-    10: 109,
-    11: 123,
-    12: 111,
-    13: 43,
-    14: 52,
-    15: 99,
-    16: 128,
-    17: 111,
-    18: 110,
-    19: 98,
-    20: 135,
-    21: 112,
-    22: 78,
-    23: 118,
-    24: 64,
-    25: 77,
-    26: 227,
-    27: 93,
-    28: 88,
-    29: 69,
-    30: 60,
-    31: 34,
-    32: 30,
-    33: 73,
-    34: 54,
-    35: 45,
-    36: 83,
-    37: 182,
-    38: 88,
-    39: 75,
-    40: 85,
-    41: 54,
-    42: 53,
-    43: 89,
-    44: 59,
-    45: 37,
-    46: 35,
-    47: 38,
-    48: 29,
-    49: 18,
-    50: 45,
-    51: 60,
-    52: 49,
-    53: 62,
-    54: 55,
-    55: 78,
-    56: 96,
-    57: 29,
-    58: 22,
-    59: 24,
-    60: 13,
-    61: 14,
-    62: 11,
-    63: 11,
-    64: 18,
-    65: 12,
-    66: 12,
-    67: 30,
-    68: 52,
-    69: 52,
-    70: 44,
-    71: 28,
-    72: 28,
-    73: 20,
-    74: 56,
-    75: 40,
-    76: 31,
-    77: 50,
-    78: 40,
-    79: 46,
-    80: 42,
-    81: 29,
-    82: 19,
-    83: 36,
-    84: 25,
-    85: 22,
-    86: 17,
-    87: 19,
-    88: 26,
-    89: 30,
-    90: 20,
-    91: 15,
-    92: 21,
-    93: 11,
-    94: 8,
-    95: 8,
-    96: 19,
-    97: 5,
-    98: 8,
-    99: 8,
-    100: 11,
-    101: 11,
-    102: 8,
-    103: 3,
-    104: 9,
-    105: 5,
-    106: 4,
-    107: 7,
-    108: 3,
-    109: 6,
-    110: 3,
-    111: 5,
-    112: 4,
-    113: 5,
-    114: 6
+    1: 7, 2: 286, 3: 200, 4: 176, 5: 120, 6: 165, 7: 206, 8: 75, 9: 129,
+    10: 109, 11: 123, 12: 111, 13: 43, 14: 52, 15: 99, 16: 128, 17: 111, 18: 110,
+    19: 98, 20: 135, 21: 112, 22: 78, 23: 118, 24: 64, 25: 77, 26: 227, 27: 93,
+    28: 88, 29: 69, 30: 60, 31: 34, 32: 30, 33: 73, 34: 54, 35: 45, 36: 83,
+    37: 182, 38: 88, 39: 75, 40: 85, 41: 54, 42: 53, 43: 89, 44: 59, 45: 37,
+    46: 35, 47: 38, 48: 29, 49: 18, 50: 45, 51: 60, 52: 49, 53: 62, 54: 55,
+    55: 78, 56: 96, 57: 29, 58: 22, 59: 24, 60: 13, 61: 14, 62: 11, 63: 11,
+    64: 18, 65: 12, 66: 12, 67: 30, 68: 52, 69: 52, 70: 44, 71: 28, 72: 28,
+    73: 20, 74: 56, 75: 40, 76: 31, 77: 50, 78: 40, 79: 46, 80: 42, 81: 29,
+    82: 19, 83: 36, 84: 25, 85: 22, 86: 17, 87: 19, 88: 26, 89: 30, 90: 20,
+    91: 15, 92: 21, 93: 11, 94: 8, 95: 8, 96: 19, 97: 5, 98: 8, 99: 8,
+    100: 11, 101: 11, 102: 8, 103: 3, 104: 9, 105: 5, 106: 4, 107: 7, 108: 3,
+    109: 6, 110: 3, 111: 5, 112: 4, 113: 5, 114: 6
 }
-
 
 EDITION = "quran-simple"
 
 def get_random_ayah():
-    sura = random.choice(list(sura_verses.keys()))
-    ayah = random.randint(1, sura_verses[sura])
-    url = f"http://api.alquran.cloud/v1/ayah/{sura}:{ayah}/{EDITION}"
-    try:
-        res = requests.get(url, timeout=5)
-        data = res.json()
-        text = data.get("data", {}).get("text", "not found")
-        surah_name = data.get("data", {}).get("surah", {}).get("name", "")
-        number = data.get("data", {}).get("numberInSurah", "")
-        return f"{text}\n\nسوره: {surah_name}, آیه: {number}"
-    except Exception as e:
-        return f"Error: {e}"
+    """گرفتن یک آیه تصادفی بدون سجده"""
+    while True:
+        sura = random.choice(list(sura_verses.keys()))
+        ayah = random.randint(1, sura_verses[sura])
+        url = f"http://api.alquran.cloud/v1/ayah/{sura}:{ayah}/{EDITION}"
+        try:
+            res = requests.get(url, timeout=5)
+            data = res.json()
+            if data.get("data", {}).get("sajda"):  # اگر آیه سجده‌دار بود دوباره بگیر
+                continue
+            text = data.get("data", {}).get("text", "متنی موجود نیست")
+            surah_name = data.get("data", {}).get("surah", {}).get("name", "")
+            number = data.get("data", {}).get("numberInSurah", "")
+            return f"{text}\n\nسوره: {surah_name}, آیه: {number}"
+        except Exception as e:
+            return f"خطا در دریافت آیه: {e}"
 
 class QuranApp(Gtk.Window):
     def __init__(self):
-        super().__init__(title="holy quran")
+        super().__init__(title="📖 قرآن کریم")
         self.set_default_size(700, 500)
         self.set_border_width(20)
 
+        # CSS زیبا
         css_provider = Gtk.CssProvider()
         css = """
-        window {
-            background-color: #011627;
-        }
+        window { background-color: #011627; }
         textview {
             background-color: rgba(20,30,50,0.85);
             color: #d6deeb;
@@ -163,25 +64,30 @@ class QuranApp(Gtk.Window):
             padding: 10px;
             font-weight: bold;
         }
-        button:hover {
-            background-color: #50bfa0;
-        }
+        button:hover { background-color: #50bfa0; }
+        label.info { color: #ffd166; font-weight: bold; font-size: 14px; }
         """
         css_provider.load_from_data(css.encode())
         Gtk.StyleContext.add_provider_for_screen(
             Gdk.Screen.get_default(), css_provider, Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION
         )
 
-        vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=20)
+        vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=15)
         self.add(vbox)
 
-        self.label = Gtk.Label(label="--- holy quran ---")
+        # توضیح کوتاه
+        self.info_label = Gtk.Label(label="📌 توجه: آیات سجده‌دار نمایش داده نمی‌شوند.")
+        self.info_label.get_style_context().add_class("info")
+        vbox.pack_start(self.info_label, False, False, 0)
+
+        # عنوان
+        self.label = Gtk.Label(label="📖 قرآن کریم")
         self.label.set_justify(Gtk.Justification.CENTER)
         self.label.modify_font(Pango.FontDescription("Estedad 28"))
         self.label.set_halign(Gtk.Align.CENTER)
-        self.label.set_valign(Gtk.Align.START)
         vbox.pack_start(self.label, False, False, 0)
 
+        # TextView برای نمایش آیه
         self.textview = Gtk.TextView()
         self.textview.set_editable(False)
         self.textview.set_cursor_visible(False)
@@ -189,7 +95,8 @@ class QuranApp(Gtk.Window):
         self.textview.modify_font(Pango.FontDescription("Estedad 20"))
         vbox.pack_start(self.textview, True, True, 0)
 
-        self.button = Gtk.Button(label="next")
+        # دکمه بعدی
+        self.button = Gtk.Button(label="آیه بعدی ➡️")
         self.button.connect("clicked", self.on_button_click)
         vbox.pack_start(self.button, False, False, 0)
 
